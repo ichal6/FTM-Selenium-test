8a7ec79ca440302637d4166957d37d991ccd4545
Michael Lechowicz <ichal.lechowicz@gmail.com>
2022-06-08 13:55:29 +0200

    Add test for try login with incorrect credentials

--- Enter-pages/ftm_login.py
+++ Enter-pages/ftm_login.py
@@ -1,4 +1,3 @@
-import time
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -26,6 +25,21 @@ class FollowTheMoneyTest(unittest.TestCase):
                 EC.presence_of_element_located((By.XPATH, '//header/div[1]/a[1]/p'))).text
 
         self.assertEqual("User Userowy", user_name)
+
+    def test_login_with_wrong_credentials(self):
+        driver = self.driver
+        driver.get("https://ftm-client.herokuapp.com/login")
+
+        email = driver.find_element(By.NAME, "email")
+        password = driver.find_element(By.NAME, "password")
+        email.send_keys("bad@user.pl")
+        password.send_keys("bad2")
+        password.send_keys(Keys.RETURN)
+
+        warning = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'warning'))).text
+
+        self.assertEqual("Your username or password was not recognised - try again.", warning)
 
     def tearDown(self):
         self.driver.close()
